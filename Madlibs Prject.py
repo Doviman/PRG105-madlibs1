9826d73360f11f5966d134172c535de87e9c30d1
Doviman <disaacman@students.mchenry.edu>
2016-08-30 22:10:12 -0500

    Add files via upload

---
+++ Madlibs Prject.py
@@ -0,0 +1,33 @@
+# Variables
+person = "Rambo"
+part1 = "fist"
+part2 = "missle launcher"
+action1 = "chew"
+action2 = "spit"
+adverb = "fiesty"
+noun = "ninja warrior"
+
+
+
+
+
+
+
+
+
+
+
+
+# Poem
+print(person + " is a " + adverb + " " + noun + ",")
+print("Short and stout,")
+print("Here is a " + person + " " + part1 + ",")
+print("Here is a " + person + " " + part2 + ",")
+print("When " + person + " get all steamed up,")
+print("Hear " + person + " shout,")
+print(action1 + " " + person + " up and " + action2 + " " + person + " out! ")
+print(person + " is a clever " + noun + ",")
+print("Yes, it's true,")
+print("Here's an example of what " + person + " can do,")
+print(person + " can turn his " + part1 + " into a " + part2 + ",")
+print(action1 + " " + person + " over and " + action2 + " " + person + " out!")
